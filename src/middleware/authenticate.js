4bb7fca3f979f2e2a53293e8d7ee90b92801e0ce
Michael Harrison <harrim91@hotmail.co.uk>
2018-11-21 20:54:21 +0000

    More debugging

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -3,16 +3,22 @@ const jwt = require('jsonwebtoken');
 exports.authenticate = (req, res, next) => {
   const token = req.get('Authorization');
 
+  console.log('token', token);
+
   if (token) {
     jwt.verify(token, process.env.JWT_SECRET, (error, authorizer) => {
+      console.error('verify error', error);
       if (error) {
         res.status(401).json({ message: 'Unable to authenticate token' });
       } else {
+        console.error('request authorizer', authorizer);
         req.authorizer = { id: authorizer._id };
         next();
       }
     });
   } else {
+    console.log('no token');
+
     res.status(401).json({ message: 'No token provided' });
   }
 };
